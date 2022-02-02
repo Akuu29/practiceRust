1451ae573d289f9408fb7c4c02f28dcb63104af6
Akuu29 <owner.akiu0529@gmail.com>
2022-02-02 13:52:24 +0900

    Add Add struct Card && fn get_cards_string

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,22 @@
+#[derive(Debug)]
+
 struct Card {
     suit: String,
     value: String,
-    int_value: u64
-}
-
-fn build_card(suit: String, value: String, int_value: u64) -> Card {
-    Card {
-        suit,
-        value,
-        int_value
-    }
+    int_value: u32
 }
 
 impl Card {
-    fn getCardString(&self) -> self {
-        
+    fn get_cards_string(&self) -> String {
+        format!("{} {} ({})", self.suit, self.value, self.int_value.to_string())
     }
 }
 
 fn main() {
-    let card1 = build_card (
-        String::from("♦︎"),
-        String::from("A"),
-        1
-    );
-
-    // let a = card1.getCardString();
-
-    // dbg!(a);
+    let card1 = Card {
+        suit: String::from("♦︎"),
+        value: String::from("A"),
+        int_value: 1,
+    };
+    println!("{}", card1.get_cards_string());
 }
