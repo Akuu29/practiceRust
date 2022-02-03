af43364e8c68b97ec16a2dc562f7d9623e186e43
Akuu29 <owner.akiu0529@gmail.com>
2022-02-03 15:47:07 +0900

    Add Add struct Deck, method generate_deck

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,34 @@ impl Card {
     }
 }
 
+struct Deck {
+    deck: Vec<Card>,
+}
+
+impl Deck {
+    fn generate_deck() -> Deck {
+        let mut new_deck = vec![];
+        let suits = ["♣", "♦", "♥", "♠"];
+        let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+        let mut int_value = 1;
+        for suit in suits.iter() {
+            for value in values.iter() {
+                let card = Card {
+                    suit: suit.to_string(),
+                    value: value.to_string(),
+                    int_value: int_value
+                };
+                new_deck.push(card);
+                int_value += 1;
+            }
+        }
+
+        Deck {deck: new_deck}
+    }
+}
+
 fn main() {
-    let card1 = Card {
-        suit: String::from("♦︎"),
-        value: String::from("A"),
-        int_value: 1,
-    };
-    println!("{}", card1.get_cards_string());
+    let deck = Deck::generate_deck();
+    println!("{:?}", deck.deck);
 }
